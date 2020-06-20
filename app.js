cce654c18d09dc2f2f3890257bea3d9d26f6991b
David Rogers <davidrogersau@yahoo.com.au>
2020-06-20 13:08:15 +1000

    Fixed the Patch route code.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-//const db = mongoose.connect('mongodb://192.168.1.110:27017/tvshowtracker', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connect('mongodb+srv://dave:890iop)P@kesselrundb-v4cz9.mongodb.net/tvshowtracker?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connect('mongodb://192.168.1.110:27017/tvshowtracker', { useNewUrlParser: true, useUnifiedTopology: true });
+//const db = mongoose.connect('mongodb+srv://dave:890iop)P@kesselrundb-v4cz9.mongodb.net/tvshowtracker?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const port = process.env.PORT || 80;
 const TvShow = require('./models/tvShowModel');
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -27,6 +27,66 @@ function routes(TvShow) {
             });
         });
 
+    appRouter.use('/tvshows/:showId', (req, res, next) => {
+
+        TvShow.findById(req.params.showId, (err, tvShow) => {
+            if (err) {
+                return res.send(err);
+            }
+
+            if (tvShow) {
+                req.tvShow = tvShow;
+                return next();
+            }
+
+            return res.sendStatus(404);
+        });
+
+    });
+
+    appRouter.route('/tvshows/:showId')
+        .get((req, res) => { res.json(req.tvShow) })
+        .put((req, res) => {
+            const { tvShow } = req;
+
+            tvShow.title = req.body.title;
+            tvShow.author = req.body.author;
+            tvShow.genre = req.body.genre;
+            tvShow.read = req.body.read;
+
+            tvShow.save();
+
+            return res.json(tvShow);
+
+        })
+        .patch((req, res) => {
+            const { tvShow } = req;
+
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            Object.entries(req.body).forEach(item => {
+                const key = item[0];
+                const value = item[1];
+                tvShow[key] = value;
+            });
+
+            req.tvShow.save((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+
+                return res.json(tvShow)
+            });
+        }).delete((req, res) => {
+            req.tvShow.remove((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.sendStatus(204);
+            });
+        });
 
     return appRouter;
 
